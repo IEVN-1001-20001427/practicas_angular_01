d00478589b1319dabd0c9a89961852fcd9698c22
Angel Moreno <20001427@alumnos.utleon.edu.mx>
2023-09-27 18:28:29 -0600

    27-09-2023 JAME

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practicas_angular_01';
+
+  operacion:string = '';
+  temp:string = '';
+  resultado:number = 0;
+
+  calcular():void{
+    switch(this.operacion){
+      case 'c':
+        this.cent();
+        break;
+      case 'f':
+        this.far();
+        break;
+      default: break;
+    }
+  }
+
+  cent():void{
+    this.resultado = (parseInt(this.temp) - 32) * (5/9);
+  }
+
+  far():void{
+    this.resultado = (parseInt(this.temp)* (9/5)) + 32 ;
+  }
+
 }
